8ef3cb2c0ab2ba948a5dc9052ac0a78d0a25e105
Collin Schneider <collin@jastr.co>
2016-10-11 14:22:30 -0400

    links

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
   get 'contact' => 'welcome#contact'
+  get 'about' => 'welcome#about'
+  get 'shows' => 'welcome#shows'
   resources :mp3s
 
   # Example of regular route:
